cec32672a52912e151a8aed9873b70be43896051
Jonathan Wilchield Arthur <100198275+jocypher@users.noreply.github.com>
2026-01-21 15:00:43 +0000

    update: worked on the logs controller

--- src/controllers/LogController.ts
+++ src/controllers/LogController.ts
@@ -1,6 +1,13 @@
-import User from "../models/user-model.entity"
+import {User} from "../models/user-model.entity"
+import AppDataSource from "../configs/app-datasource.config"
+import { SubComponent } from "../models/organization-service.entity"
+import {Log} from "../models/log-item.entity"
+import organizationService from "../services/organization.service"
+import { OrganizationUser } from "../models/organization-user.entity"
 const userRepo = AppDataSource.getRepository(User)
-const orgServiceRepo = AppDatasource.getRepository(SubComponent)
+const orgUserRepo = AppDataSource.getRepository(OrganizationUser)
+const orgServiceRepo = AppDataSource.getRepository(SubComponent)
+const logRepo = AppDataSource.getRepository(Log)
 // in the logs controller, we will display the logs 
 // within the service so the question is how should the logs be like 
 // I know the frontend is expected to do that but the question is how should it be displayed .
@@ -10,25 +17,41 @@ const userId = req.id
 const {orgId,serviceId} = req.params
 const {logMessage, logStatus, logTag} = req.body
 
-if(!logMessage || !logStatus | !logTag){
+if(!logMessage || !logStatus || !logTag){
     return res.status(400).json({message: "Required field parameters"})
 }
  try{
-   const user = await userRepo.findOne({
-    wher:{
+   const orgUser = await orgUserRepo.findOne({
+    where:{
       id: userId
     }
    })
-   if(!user){
+   if(!orgUser || !organizationService.isOrgAdminOrOwner(orgUser)){
     return res.status(404).json({message:"User Not found"})
    }
 
-   const orgService = orgServiceRepo.findOne({
+   const orgService = await orgServiceRepo.findOne({
     where:{
       id: serviceId,
       organization: {id: orgId}
     }
    })
+
+   if(!orgService) return res.status(401).json({message: "The organization with the service isn't available"})
+
+
+    const createManualLogs = logRepo.create({
+      message: logMessage,
+      logLevel: logStatus,
+      tags: logTag,
+      created_by: orgUser.user,
+      sub_component: orgService,
+
+    })
+
+    const logs =  await logRepo.save(createManualLogs)
+
+    return res.status(200).json({message: `Logs created `})
    
 
  }catch(err:any){
