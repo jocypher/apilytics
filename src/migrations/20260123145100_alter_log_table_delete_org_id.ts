566596913c725d890594e8c379028a12e68c5ec5
Jonathan Wilchield Arthur <100198275+jocypher@users.noreply.github.com>
2026-01-23 15:15:12 +0000

    fix: log table migrated

---
+++ src/migrations/20260123145100_alter_log_table_delete_org_id.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AlterLogTableDeleteOrgId2026012345100 implements MigrationInterface{
+  
+   public async up(queryRunner: QueryRunner): Promise<any> {
+       await queryRunner.query(`ALTER TABLE "log" DROP COLUMN "organization_id"`)
+    }
+  public async  down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "log" ADD COLUMN "organization_id" SET NULL`)
+    }
+    
+}
